0eb134c6e348ee6d207a1260647c9b7f67a50fac
JacksonJLGravino <jacksonjlgravino@gmail.com>
2026-06-25 07:57:05 -0300

    desafio 7 preparando pacotes

---
+++ Desafio07_agendamento_petshop/src/main.js
@@ -0,0 +1,10 @@
+"use strict";
+
+import "./libs/dayjs.js";
+
+// CSS
+import "./styles/global.css";
+import "./styles/extremes.css";
+import "./styles/header.css";
+import "./styles/schedule.css";
+import "./styles/modal.css";
