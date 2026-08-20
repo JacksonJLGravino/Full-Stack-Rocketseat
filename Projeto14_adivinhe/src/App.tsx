3fbc99405fe573ce155b11c3d78b311e7d3ea0be
JacksonJLGravino <jacksonjlgravino@gmail.com>
2026-08-20 19:54:41 -0300

    projeto 14 interface

--- Projeto14_adivinhe/src/App.tsx
+++ Projeto14_adivinhe/src/App.tsx
@@ -1,5 +1,10 @@
 import { Header } from "./components/Header";
 import styles from "./app.module.css";
+import { Tip } from "./components/Tip";
+import { Letter } from "./components/Letter";
+import { Input } from "./components/Input";
+import { Button } from "./components/Button";
+import { LettersUsed } from "./components/LettersUsed";
 
 export default function App() {
   function handleRestartGame() {
@@ -8,7 +13,25 @@ export default function App() {
 
   return (
     <div className={styles.container}>
-      <Header current={5} max={10} onRestart={handleRestartGame}></Header>
+      <main>
+        <Header current={5} max={10} onRestart={handleRestartGame} />
+        <Tip tip="linguagem de programação dinâmica" />
+        <div className={styles.word}>
+          <Letter value="R" />
+          <Letter value="E" />
+          <Letter value="A" />
+          <Letter value="C" />
+          <Letter value="T" />
+        </div>
+
+        <h4>Palpite</h4>
+        <div className={styles.guess}>
+          <Input autoFocus maxLength={1} placeholder="?" />
+          <Button title="Confirmar" />
+        </div>
+
+        <LettersUsed />
+      </main>
     </div>
   );
 }
